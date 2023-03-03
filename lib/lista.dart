4f999804df59c95c4d261ff27dc0035e93a70c2b
frankpezo <frankanderpg.2000@gmail.com>
2023-03-02 22:18:32 -0500

    Consumiendo api utilizando método GET para mostrar los datos en la app flutter

--- lib/lista.dart
+++ lib/lista.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class ListDatos extends StatefulWidget {
@@ -12,17 +11,21 @@ class ListDatos extends StatefulWidget {
 }
 
 class _ListDatosState extends State<ListDatos> {
+  //3.2. Creamos una lista para obtener las respuestas
+  List userData = [];
   //1. Declaramos la función
   Future<void> getDatos() async {
-    //3.2. Creamos una lista para obtener las respuestas
-    List userData = [];
     //2. Traemos el link
-    Uri url = Uri.parse("http://10.0.2.2/crudFlutter/viewdata.php");
+    Uri url = Uri.parse("http://10.0.2.2/crudFlutter/viewdato.php");
+    //3.3. Hacemos un setState para actualizar la lista
     //3. Hacemos la petición
     try {
       //3.1. Hacemos la petición por método get
       var response = await http.get(url);
-      userData = jsonDecode(response.body);
+      //3.2. Utilizamos un setState
+      setState(() {
+        userData = jsonDecode(response.body);
+      });
     } catch (e) {
       print(e);
     }
@@ -31,6 +34,7 @@ class _ListDatosState extends State<ListDatos> {
   //1.1. Hacemos el void init
   @override
   void initState() {
+    // ignore: todo
     // TODO: implement initState
     getDatos();
     super.initState();
@@ -43,7 +47,19 @@ class _ListDatosState extends State<ListDatos> {
         title: Text('Lista de datos'),
       ),
       body: Container(
-        child: Text('Lista de datos'),
+        child: ListView.builder(
+            itemCount: userData.length,
+            itemBuilder: (context, index) {
+              return Card(
+                margin: EdgeInsets.all(10),
+                child: ListTile(
+                  //Cuando utilizamos el método Get, debemos colocar el nombre de la columna
+                  //pero como está escrita en la base de datos
+                  title: Text(userData[index]['uname'].toString()),
+                  subtitle: Text(userData[index]['uemail'].toString()),
+                ),
+              );
+            }),
       ),
     );
   }
